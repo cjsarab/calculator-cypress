e9ac62f22c1afa128befa161ddc7925464ef9d2d
Cammy Smith <cjsarab1991@gmail.com>
2022-10-17 15:14:36 +0100

    Most done

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -28,7 +28,6 @@ describe('Calculator', () => {
     fireEvent.click(equals);
 
     expect(runningTotal.textContent).toEqual('5');
-
   });
 
   it ('should be able to subtract numbers from one another', () => {
@@ -44,9 +43,69 @@ describe('Calculator', () => {
     fireEvent.click(equals);
 
     expect(runningTotal.textContent).toEqual('3');
-
-
-
   })
+
+  it ('should be able to multiply two numbers together', () => {
+    const button3 = container.getByTestId('number3');
+    const button5 = container.getByTestId('number5');
+    const multiply = container.getByTestId('operator-multiply');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+
+    fireEvent.click(button3);
+    fireEvent.click(multiply);
+    fireEvent.click(button5);
+    fireEvent.click(equals);
+
+    expect(runningTotal.textContent).toEqual('15');
+  });
+
+  it ('should be able to divide one numbers by another', () => {
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1');
+    const button7 = container.getByTestId('number7');
+    const divide = container.getByTestId('operator-divide');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(divide);
+    fireEvent.click(button7);
+    fireEvent.click(equals);
+
+    expect(runningTotal.textContent).toEqual('3');
+  });
+
+  it ('should be able to concatenate multiple number button clicks', () => {
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1');
+    const button7 = container.getByTestId('number7');
+    const runningTotal = container.getByTestId('running-total');
+
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(button7);
+
+    expect(runningTotal.textContent).toEqual('217');
+  });
+
+  it ('should be able to chain multiple operations together', () => {
+    const button1 = container.getByTestId('number1');
+    const button2 = container.getByTestId('number2');
+    const add = container.getByTestId('operator-add');
+    const equals = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+
+    fireEvent.click(button2);
+    fireEvent.click(add);
+    fireEvent.click(button1);
+    fireEvent.click(add);
+    fireEvent.click(button2);
+    fireEvent.click(equals);
+
+    expect(runningTotal.textContent).toEqual('5');
+  });
+
 
 });
